797f55238b910e677283726e6a71dfe374362af0
LRboy <603552916@qq.com>
2023-07-24 14:46:50 +0800

    [fix]:删掉多与嗲吗

--- sanity/schema.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity';
-import post, { readingTimeType } from './schemas/post';
-import category from './schemas/category';
-import blockContent from './schemas/blockContent';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [readingTimeType, post, category, blockContent],
-};
