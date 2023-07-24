5e066c81d4518372a7bfda3e153c255015a5d132
LRboy <603552916@qq.com>
2023-07-24 14:51:27 +0800

    [fix]:修复获取国际化文件路径错误的问题

--- i18n.server.ts
+++ i18n.server.ts
@@ -1,4 +1,4 @@
 export async function getMessages({ locale }: RootParams) {
-  const { default: messages } = await import(`~/messages/${locale}.json`);
+  const { default: messages } = await import(`./messages/${locale}.json`);
   return messages;
 }
