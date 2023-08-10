fc4f7bb3d645a96c5fa52af00a3414c4ec898d0e
LRboy <603552916@qq.com>
2023-08-10 20:31:08 +0800

    添加生产环境配置

--- sanity/config.ts
+++ sanity/config.ts
@@ -1,12 +1,26 @@
 export const useCDN = process.env.NODE_ENV === 'production'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || ('production' as string)
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
 
 // see https://www.sanity.io/docs/api-versioning for how versioning works
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-03-25'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-24'
 
 // This is the document id used for the preview secret that's stored in your dataset.
 // The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
 export const previewSecretId = process.env.SANITY_REVALIDATE_SECRET as string
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
