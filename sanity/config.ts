b78cd4cc8245f85bd9cacd94dc641234e50ea698
LRboy <603552916@qq.com>
2023-08-10 20:21:21 +0800

    fix:添加默认值

--- sanity/config.ts
+++ sanity/config.ts
@@ -1,6 +1,6 @@
 export const useCDN = process.env.NODE_ENV === 'production'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || ('production' as string)
 
